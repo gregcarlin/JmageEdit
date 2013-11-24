983f2ce39c5cff2eb2051dab161a77fba7163085
Greg Carlin <gregthegeek@optonline.net>
2013-11-24 10:37:12 -0500

    Starting image display

--- src/en/jmageedit/view/View.java
+++ src/en/jmageedit/view/View.java
@@ -5,12 +5,15 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 import en.jmageedit.model.Model;
 import en.jmageedit.model.filters.Filter;
@@ -19,6 +22,7 @@ public class View {
     private static final int START_WIDTH = 500;
     private static final int START_HEIGHT = 250;
     private final JFrame imageFrame = new JFrame();
+    private final JLabel imageLbl = new JLabel();
     
     private final JMenuBar menuBar = new JMenuBar();
     
@@ -64,12 +68,26 @@ public class View {
         
         imageFrame.setTitle("JmageEdit");
         imageFrame.setBounds(getCentered(START_WIDTH, START_HEIGHT));
-        imageFrame.setLayout(null); // very bad. do not do
+        imageFrame.setLayout(null); // very bad. do not do.
         JLabel lbl = new JLabel("Open an image above.");
         lbl.setBounds(START_WIDTH / 2 - 70, START_HEIGHT / 2 - 30, 200, 50);
         imageFrame.add(lbl);
+        
+        imageLbl.setVisible(true);
+        imageFrame.add(imageLbl);
+        
         imageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         imageFrame.setVisible(true);
+    }
+    
+    public void setImage(BufferedImage img) {
+        if(!imageLbl.isVisible()) imageLbl.setVisible(true);
+        
+        int width = img.getWidth();
+        int height = img.getHeight();
+        
+        imageLbl.setBounds(0, 0, width, height);
+        imageLbl.setIcon(new ImageIcon(img));
     }
     
     /**
